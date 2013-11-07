ffa6fd0a5a5371670f1dd40b0e51de99d01470cd
Ian Tewksbury <itewksbu@redhat.com>
2013-11-07 16:58:55 -0500

    initial commit of the windup.ui plugin with the framework for creating a windup report view in Eclipse.

---
+++ plugins/org.jboss.tools.windup.ui/src/org/jboss/tools/windup/ui/views/WindupReportView.java
@@ -0,0 +1,30 @@
+package org.jboss.tools.windup.ui.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+/**
+ * <p>
+ * A view to display Windup Reports.
+ * </p>
+ */
+public class WindupReportView extends ViewPart {
+
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "org.jboss.tools.windup.ui.views.WindupReportView";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+}
