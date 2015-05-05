79ad4b40790932c8a0c1505ace7d2c0836ad8847
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2015-05-05 10:12:11 -0400

    Fix NPE in MarkerContributionFactory

--- plugins/org.jboss.tools.windup.ui/src/org/jboss/tools/windup/ui/markers/MarkerContributionFactory.java
+++ plugins/org.jboss.tools.windup.ui/src/org/jboss/tools/windup/ui/markers/MarkerContributionFactory.java
@@ -1,5 +1,6 @@
 package org.jboss.tools.windup.ui.markers;
 
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.menus.ExtensionContributionFactory;
 import org.eclipse.ui.menus.IContributionRoot;
@@ -11,8 +12,11 @@ public class MarkerContributionFactory extends ExtensionContributionFactory
     @Override
     public void createContributionItems(IServiceLocator serviceLocator, IContributionRoot additions)
     {
-        EditorPart editor = (EditorPart) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
-
-        additions.addContributionItem(new MarkerMenuContribution(editor), null);
+        IWorkbenchPart activePart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
+        if (activePart instanceof EditorPart)
+        {
+            EditorPart editor = (EditorPart) activePart;
+            additions.addContributionItem(new MarkerMenuContribution(editor), null);
+        }
     }
 }
