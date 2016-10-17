24e36d906ba879fef85ab14ed5918d9fbecfeaf6
John Steele <josteele@redhat.com>
2016-10-17 16:35:06 -0700

    WINDUP-1194 - Created intro content for the Windup Eclipse tooling.

--- plugins/org.jboss.tools.windup.ui/src/org/jboss/tools/windup/ui/intro/ShowGettingStartedAction.java
+++ plugins/org.jboss.tools.windup.ui/src/org/jboss/tools/windup/ui/intro/ShowGettingStartedAction.java
@@ -17,7 +17,11 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.intro.IIntroManager;
+import org.eclipse.ui.intro.IIntroPart;
 import org.eclipse.ui.progress.UIJob;
+import org.jboss.tools.windup.ui.WindupPerspective;
 import org.jboss.tools.windup.ui.WindupUIPlugin;
 import org.jboss.tools.windup.ui.internal.Messages;
 import org.jboss.tools.windup.ui.intro.GettingStartedEditor.EditorInput;
@@ -33,12 +37,21 @@ public class ShowGettingStartedAction implements IWorkbenchWindowActionDelegate 
 		ShowGettingStartedAction.doRun(window);
 	}
 
+	/**
+	 * Closes the intro view, opens the Windup perspective, and opens the 'getting started' editor.
+	 */
 	public static void doRun(IWorkbenchWindow window) {
 		UIJob job = new UIJob(Messages.showWindupGettingStarted) {
 			public IStatus runInUIThread(IProgressMonitor monitor) {
 				monitor.beginTask(Messages.showWindupGettingStarted, 1);
 				try {
 					try {
+						IIntroManager introMgr = window.getWorkbench().getIntroManager();
+						IIntroPart intro = introMgr.getIntro();
+						if (intro != null) {
+							introMgr.closeIntro(intro);
+						}
+						PlatformUI.getWorkbench().showPerspective(WindupPerspective.ID, window);
 						monitor.worked(1);
 						window.getActivePage().openEditor(EditorInput.INSTANCE, GettingStartedEditor.VIEW_ID);
 						return Status.OK_STATUS;
